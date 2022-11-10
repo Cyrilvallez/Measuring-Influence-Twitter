d117d2345bbbe74337aaf741eab5ef2351847f89
Cyril Vallez <cyril.vallez@epfl.ch>
2022-11-09 19:23:19 -0500

    Update julia_requirements.jl

--- julia_requirements.jl
+++ julia_requirements.jl
@@ -9,17 +9,17 @@ packages_with_version = [
     ("Graphs", "1.7.4"),
     ("GraphPlot", "0.5.2"),
     ("JSON", "0.21.3"),
-    ("PlotlyBase", "0.8.19"),
-    ("PlotlyJS", "0.18.9"),
     ("Plots", "1.34.4"),
     ("Pluto", "0.19.9"),
-    ("PlutoPlotly", "0.3.4"),
     ("PlutoUI", "0.7.43"),
     ("PyPlot", "2.11.0"),
     ("SimpleWeightedGraphs", "1.2.1"),
     ("StatsBase", "0.33.21"),
     ("WordCloud", "0.10.7"),
-    ("URIs", "1.4.0")
+    ("URIs", "1.4.0"),
+    ("PlotlyJS", "0.18.9"),
+    ("PlotlyBase", "0.8.19"),
+    ("PlutoPlotly", "0.3.4")
 ]
 
 packages_without_version = [
