0037aff4e09d8ba432552d627509976d2af72867
Cyril Vallez <cyril.vallez@epfl.ch>
2023-01-24 23:54:06 -0500

    Update test_speed.jl

--- Julia/test_speed.jl
+++ Julia/test_speed.jl
@@ -9,7 +9,7 @@ igg = InfluenceGraphGenerator(Engine.JointDistanceDistribution, surrogate=nothin
 f = (x,y) -> pvalue(jdd(OneSampleTTest, x, y, B=10, D=5, τ=1, μ0=0.0), tail=:right) < 0.001 ? 1 : 0
 f2 = (x,y) -> igg.causal_function(x,y)
 
-x = rand(600)
-y = rand(600)
+x = rand(200)
+y = rand(200)
 
 @btime f2(x,y)
